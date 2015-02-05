bed390c91f14bb895b416cded445d89a6fe819ca
chancier <306965810@qq.com>
2015-02-05 11:54:38 +0800

    layer 操作的部分demo

--- Autolayout/Autolayout/ViewController.h
+++ Autolayout/Autolayout/ViewController.h
@@ -10,6 +10,7 @@
 
 @interface ViewController : UIViewController
 
-
+@property (weak, nonatomic) IBOutlet UIImageView *testView;
+@property (strong, nonatomic) UIView *customView;
 @end
 
--- Autolayout/Autolayout/ViewController.m
+++ Autolayout/Autolayout/ViewController.m
@@ -65,6 +65,54 @@
     [self.view addConstraint:constraint];
 }
 
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+
+    //通过uiview设置（2D效果）
+    //    self.iconView.transform=CGAffineTransformMakeTranslation(0, -100);
+    //通过layer来设置（3D效果,x，y，z三个方向）
+    //    self.iconView.layer.transform=CATransform3DMakeTranslation(100, 20, 0);
+  
+    //通过KVC来设置
+    NSValue *v=[NSValue valueWithCATransform3D:CATransform3DMakeTranslation(100, 20, 0)];
+    [self.testView.layer setValue:v forKeyPath:@"transform"];
+    
+    //设置阴影的颜色
+    self.testView.layer.shadowColor=[UIColor redColor].CGColor;
+    //设置阴影的偏移量，如果为正数，则代表为往右边偏移
+    self.testView.layer.shadowOffset=CGSizeMake(0, 15);
+    //设置阴影的透明度(0~1之间，0表示完全透明)
+    self.testView.layer.shadowOpacity=0.5;
+    
+    //如果是只需要设置在某一个方向上的移动，可以参考下面的代码
+    //在x的方向上向左移动100
+//    [self.testView.layer setValue:@(100) forKeyPath:@"transform.translation.x"];
+}
+
+- (void)setLayer
+{
+    //设置边框的宽度为20
+    self.customView.layer.borderWidth=5;
+    //设置边框的颜色
+    self.customView.layer.borderColor=[UIColor blackColor].CGColor;
+    //设置layer的圆角
+    self.customView.layer.cornerRadius=20;
+    //设置超过子图层的部分裁减掉
+    //UI框架中使用的方法
+    //    self.customView.clipsToBounds=YES;
+    self.customView.layer.masksToBounds=YES;
+  
+    //在view的图层上添加一个image，contents表示接受内容
+    self.customView.layer.contents=(id)[UIImage imageNamed:@"me"].CGImage;
+    
+    //设置阴影的颜色
+    self.customView.layer.shadowColor=[UIColor blackColor].CGColor;
+    //设置阴影的偏移量，如果为正数，则代表为往右边偏移
+    self.customView.layer.shadowOffset=CGSizeMake(15, 5);
+    //设置阴影的透明度(0~1之间，0表示完全透明)
+    self.customView.layer.shadowOpacity=0.6;
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
